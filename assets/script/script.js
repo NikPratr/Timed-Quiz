c061d4283447cc000ced824d6cf39843fa26089c
Nick Prater <NikPratr@gmail.com>
2022-09-18 14:06:15 -0400

    Fixing timer error, quiz finished

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,34 +1,122 @@
-// var display     = document.querySelector("#q-box");
 var questionArr = [
     {
-        prompt: "What is your favorite color?",
-        choiceOne: "Red",
-        choiceTwo: "Yellow",
-        choiceThree: "Green",
-        choiceFour: "Blue",
-        answer: "Blue"
+        prompt: "What is the capital of the United States?",
+        choiceOne: "Olympia",
+        choiceTwo: "Washington, D.C.",
+        choiceThree: "Providence",
+        choiceFour: "Helena",
+        answer: "Washington, D.C."
     },
     {
-        prompt: "What is your favorite flavor?",
-        choiceOne: "Chocolate",
-        choiceTwo: "Vanilla",
-        choiceThree: "Strawberry",
-        choiceFour: "Orange",
-        answer: "Orange"
+        prompt: "What is the capital of the United Kingdom?",
+        choiceOne: "Brighton",
+        choiceTwo: "Edinburgh",
+        choiceThree: "Cambridge",
+        choiceFour: "London",
+        answer: "London"
     },
     {
-        prompt: "Why are you?",
-        choiceOne: "yes",
-        choiceTwo: "gqweood",
-        choiceThree: "asd",
-        choiceFour: "123",
-        answer: "123"
+        prompt: "What is the capital of Italy?",
+        choiceOne: "Venice",
+        choiceTwo: "Milan",
+        choiceThree: "Rome",
+        choiceFour: "Florence",
+        answer: "Rome"
+    },
+    {
+        prompt: "What is the capital of Germany?",
+        choiceOne: "Munich",
+        choiceTwo: "Cologne",
+        choiceThree: "Hamburg",
+        choiceFour: "Berlin",
+        answer: "Berlin"
+    },
+    {
+        prompt: "What is the capital of Sweden?",
+        choiceOne: "Gothenburg",
+        choiceTwo: "Visby",
+        choiceThree: "Stockholm",
+        choiceFour: "Helsingborg",
+        answer: "Stockholm"
+    },
+    {
+        prompt: "What is the capital of Poland?",
+        choiceOne: "Lubin",
+        choiceTwo: "Warsaw",
+        choiceThree: "Katowice",
+        choiceFour: "Kraków",
+        answer: "Warsaw"
+    },
+    {
+        prompt: "What is the capital of Russia?",
+        choiceOne: "Saint Petersburg",
+        choiceTwo: "Kazan",
+        choiceThree: "Moscow",
+        choiceFour: "Omsk",
+        answer: "Moscow"
+    },
+    {
+        prompt: "What is the capital of China?",
+        choiceOne: "Shanghai",
+        choiceTwo: "Chengdu",
+        choiceThree: "Beijing",
+        choiceFour: "Shenzhen",
+        answer: "Beijing"
+    },
+    {
+        prompt: "What is the capital of Japan?",
+        choiceOne: "Kyoto",
+        choiceTwo: "Tokyo",
+        choiceThree: "Osaka",
+        choiceFour: "Yokohama",
+        answer: "Tokyo"
+    },
+    {
+        prompt: "What is the capital of India?",
+        choiceOne: "New Delhi",
+        choiceTwo: "Mumbai",
+        choiceThree: "Agra",
+        choiceFour: "Jaipur",
+        answer: "New Delhi"
+    },
+    {
+        prompt: "What is the capital of Philippines?",
+        choiceOne: "Davao City",
+        choiceTwo: "Manila",
+        choiceThree: "Cebu City",
+        choiceFour: "Angeles",
+        answer: "Manila"
+    },
+    {
+        prompt: "What is the capital of Yemen?",
+        choiceOne: "Taizz",
+        choiceTwo: "Aden",
+        choiceThree: "Ibb",
+        choiceFour: "Sana'a",
+        answer: "Sana'a"
+    },
+    {
+        prompt: "What is the capital of Cambodia?",
+        choiceOne: "Krong Kampot",
+        choiceTwo: "Preah Sihanouk",
+        choiceThree: "Phnom Penh",
+        choiceFour: "Banlung",
+        answer: "Phnom Penh"
+    },
+    {
+        prompt: "What is the capital of Sudan?",
+        choiceOne: "Port Sudan",
+        choiceTwo: "Kassala",
+        choiceThree: "Omdurman",
+        choiceFour: "Khartoum",
+        answer: "Khartoum"
     },
 ]
 
 var sDisplay         = document.querySelector(".start-display");
 var qDisplay         = document.querySelector(".question-display");
 var eDisplay         = document.querySelector(".end-display");
+var hDisplay         = document.querySelector(".highscores-display");
 
 var question         = document.querySelector(".question");
 var startQuizBtn     = document.querySelector(".start-quiz-btn");
@@ -41,15 +129,38 @@ var incorrect        = document.querySelector("#incorrect-message");
 var quizCountdown    = document.querySelector(".timer");
 var scoreEl          = document.querySelector("#score");
 
+var submitScore      = document.querySelector(".submit-score");
+var scoreList        = document.querySelector("#highscore-list");
+var initials         = document.querySelector("#initials");
+// var storedScores     = JSON.parse(localStorage.getItem("highscoresArr"));
+
 var currentQuestion;
 var secondsRemaining = 59;
 var score            = 0;
+
+// if (storedScores !== null) {
+//     var highscoresArr = storedScores;
+// } else {var highscoresArr = []}
+
+var highscoresArr = [];
+
+if(localStorage.getItem("highscoresArr")){
+    highscoresArr = JSON.parse(localStorage.getItem("highscoresArr"))
+}
+
+
+a1.addEventListener("click", checkAnswerAndIterate)
+a2.addEventListener("click", checkAnswerAndIterate)
+a3.addEventListener("click", checkAnswerAndIterate)
+a4.addEventListener("click", checkAnswerAndIterate)
+submitScore.addEventListener("click", openHighscores)
+
 
 function timer() {
     var timeInterval = setInterval(function () {
         quizCountdown.textContent = "Time left: " + secondsRemaining;
         secondsRemaining--;
-        console.log(secondsRemaining);
+        // console.log(secondsRemaining);
     }, 1000)
 }
 
@@ -70,35 +181,62 @@ function shownQuestion(){
 }
 
 function checkAnswerAndIterate(event) {
-    console.log(event.target.textContent)
-    console.log(questionArr[currentQuestion].answer)
+    // console.log(event.target.textContent)
+    // console.log(questionArr[currentQuestion].answer)
     if(event.target.textContent == questionArr[currentQuestion].answer) {
         correct.style.display = "block";
         incorrect.style.display = "none";
         score ++;
     } else {correct.style.display = "none";
             incorrect.style.display = "block";
-            secondsRemaining -= 5;
+            secondsRemaining -= 10;
             quizCountdown.textContent = "Time left: " + secondsRemaining;
             }
 
      currentQuestion++;
     if(currentQuestion < questionArr.length) {
         shownQuestion()
-     } else if(currentQuestion >= questionArr.length || secondsRemaining <1) {
+     } else if(currentQuestion >= questionArr.length || secondsRemaining <= 0) {
         qDisplay.style.display = "none";
         eDisplay.style.display = "flex";
         scoreEl.textContent = "Your score: " + score
      }
 }
 
-//this should happen on button press
 startQuizBtn.addEventListener("click", function(event) {
     event.stopPropagation();
     startQuiz();
 })
 
-a1.addEventListener("click", checkAnswerAndIterate)
-a2.addEventListener("click", checkAnswerAndIterate)
-a3.addEventListener("click", checkAnswerAndIterate)
-a4.addEventListener("click", checkAnswerAndIterate)
+function openHighscores() {
+    eDisplay.style.display = "none";
+    hDisplay.style.display = "flex";
+    var scoreLi = document.createElement("li");
+
+// create quiz results
+    
+    var quizResults = {
+        initials: initials.value,
+        score: score
+    }
+
+// add results to highscore list and then store as string
+
+    highscoresArr.push(quizResults);
+    localStorage.setItem("highscoresArr", JSON.stringify(highscoresArr));
+
+// sort the array by score
+
+    highscoresArr.sort((a, b) => b.score - a.score)
+
+// retrieve the top 5 scores and put them on the board
+
+    for (i=0; i<highscoresArr.length; i++) {
+        if(i==5) {
+            return
+        }
+        var newLi = document.createElement("li");
+
+    newLi.textContent = highscoresArr[i].initials + ": " + highscoresArr[i].score;
+    scoreList.appendChild(newLi)
+}}
